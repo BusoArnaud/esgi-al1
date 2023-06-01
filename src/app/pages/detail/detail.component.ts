d78869b8fde28afee7984d47e6787a0d772bcff1
arnaud <pro.buso.arnaud@gmail.com>
2023-06-01 08:04:54 +0200

    first service

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,17 +1,31 @@
-import {Component} from '@angular/core';
-import {Router} from "@angular/router";
+import {Component, OnDestroy} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {Subscription} from "rxjs";
+import {RestaurantServiceService} from "../../services/restaurant-service.service";
 
 @Component({
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss']
 })
-export class DetailComponent {
+export class DetailComponent implements OnDestroy {
 
-  dishes: Array<string>;
+  dishes: Array<string> = [];
+  private routeSub: Subscription;
+
   constructor(
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute,
+    private resService: RestaurantServiceService,
   ) {
-    this.dishes = history.state.data;
+    this.routeSub = this.route.params.subscribe(params => {
+      console.log(params) //log the entire params object
+      console.log(params['id']) //log the value of id
+      this.dishes = this.resService.getFood(+params['id']);
+    });
+  }
+
+  ngOnDestroy() {
+    this.routeSub.unsubscribe();
   }
 
   backWithHistory(): void {
